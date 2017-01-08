b6713f9f6223968f6021c170112eeeac2b2bb4fe
jpryan1 <jpr349@nyu.edu>
2017-01-08 10:01:31 -0500

    Update chess.js

--- chess/chess.js
+++ chess/chess.js
@@ -1,5 +1,17 @@
 //alert("Hello World");
 //Initialize the board:
+window.onload = function () { 
 
-var BQ = document.getElementById("BQ");
-BQ.style.left = 20;
+  var BQ = document.getElementById("BQ");
+  BQ.style.left = 20+'px';
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
