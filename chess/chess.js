4a4b25ccb3bbc49554ca16860caf038d7fc41132
jpryan1 <jpr349@nyu.edu>
2017-01-20 11:07:01 -0500

    Update chess.js

--- chess/chess.js
+++ chess/chess.js
@@ -11,118 +11,118 @@ window.onload = function () {
 //INITIALIZATION - SETTING UP PIECES
 //TODO: Nothing
 function initializeBoard(){
-  move_IDX = 0;
+    move_IDX = 0;
     isReversed = false;
     
     occupied = [[1,1,1,1,1,1,1,1],
-       [1,1,1,1,1,1,1,1],
-       [0,0,0,0,0,0,0,0],
-       [0,0,0,0,0,0,0,0],
-       [0,0,0,0,0,0,0,0],
-       [0,0,0,0,0,0,0,0],
-       [2,2,2,2,2,2,2,2],
-       [2,2,2,2,2,2,2,2]
+                [1,1,1,1,1,1,1,1],
+                [0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0],
+                [2,2,2,2,2,2,2,2],
+                [2,2,2,2,2,2,2,2]
                 ];
-  var BK = new Object();
-  BK.element = document.getElementById("BK");
-  var BQ = new Object();
-  BQ.element = document.getElementById("BQ");
-  
-  
-  var BB1 = Object();
-  BB1.element = document.getElementById("BB1");
-  var BB2 = Object();
-  BB2.element = document.getElementById("BB2");
-  
-  var BP1 = Object();
-  BP1.element = document.getElementById("BP1");
-  var BP2 = Object();
-  BP2.element = document.getElementById("BP2");
-  var BP3 = Object();
-  BP3.element = document.getElementById("BP3");
-  var BP4 = Object();
-  BP4.element = document.getElementById("BP4");
-  var BP5 = Object();
-  BP5.element = document.getElementById("BP5");
-  var BP6 = Object();
-  BP6.element = document.getElementById("BP6");
-  var BP7 = Object();
-  BP7.element = document.getElementById("BP7");
-  var BP8 = Object();
-  BP8.element = document.getElementById("BP8");
-  
-  var BN1 = Object();
-  BN1.element = document.getElementById("BN1");
-  var BN2 = Object();
-  BN2.element = document.getElementById("BN2");
-  
-  var BR1 = Object();
-  BR1.element = document.getElementById("BR1");
-  var BR2 = Object();
-  BR2.element = document.getElementById("BR2");
-  
-  var WK = Object();
-  WK.element = document.getElementById("WK");
-  var WQ = Object();
-  WQ.element = document.getElementById("WQ");
-  
-  var WB1 = Object();
-  WB1.element = document.getElementById("WB1");
-  var WB2 = Object();
-  WB2.element = document.getElementById("WB2");
-  
-  var WP1 = Object();
-  WP1.element = document.getElementById("WP1");
-  var WP2 = Object();
-  WP2.element = document.getElementById("WP2");
-  var WP3 = Object();
-  WP3.element = document.getElementById("WP3");
-  var WP4 = Object();
-  WP4.element = document.getElementById("WP4");
-  var WP5 = Object();
-  WP5.element = document.getElementById("WP5");
-  var WP6 = Object();
-  WP6.element = document.getElementById("WP6");
-  var WP7 = Object();
-  WP7.element = document.getElementById("WP7");
-  var WP8 = Object();
-  WP8.element = document.getElementById("WP8");
-  
-  var WN1 = Object();
-  WN1.element = document.getElementById("WN1");
-  var WN2 = Object();
-  WN2.element = document.getElementById("WN2");
-  
-  var WR1 = Object();
-  WR1.element = document.getElementById("WR1");
-  var WR2 = Object();
-  WR2.element = document.getElementById("WR2");
-
-  blacks = [BR1, BN1, BB1, BQ, BK, BB2, BN2, BR2, BP1, BP2, BP3, BP4, BP5, BP6, BP7, BP8];
-  whites = [WR1, WN1, WB1, WQ, WK, WB2, WN2, WR2, WP1, WP2, WP3, WP4, WP5, WP6, WP7, WP8];
-  squareFun = function(row, col){ //bottom left is 1,1. 0,0 is off the board.
-			  if(row === 0 || col === 0){
-				this.element.style.display = 'none';
-			  }else{
-                          	this.element.style.top = (15+(8-row)*61.5)+'px';
-                          	this.element.style.left = (15+(col-1)*61.5)+'px';
-			  }
-                        };
-
-  for(var i=0; i<2; i++){
-    for(var j=0; j<8; j++){
-        whites[i*8+j].setSquare = squareFun;
-        whites[i*8+j].setSquare(i+1, j+1);
-        whites[i*8+j].pos = [[i+1, j+1]];
+    var BK = new Object();
+    BK.element = document.getElementById("BK");
+    var BQ = new Object();
+    BQ.element = document.getElementById("BQ");
+    
+    
+    var BB1 = Object();
+    BB1.element = document.getElementById("BB1");
+    var BB2 = Object();
+    BB2.element = document.getElementById("BB2");
+    
+    var BP1 = Object();
+    BP1.element = document.getElementById("BP1");
+    var BP2 = Object();
+    BP2.element = document.getElementById("BP2");
+    var BP3 = Object();
+    BP3.element = document.getElementById("BP3");
+    var BP4 = Object();
+    BP4.element = document.getElementById("BP4");
+    var BP5 = Object();
+    BP5.element = document.getElementById("BP5");
+    var BP6 = Object();
+    BP6.element = document.getElementById("BP6");
+    var BP7 = Object();
+    BP7.element = document.getElementById("BP7");
+    var BP8 = Object();
+    BP8.element = document.getElementById("BP8");
+    
+    var BN1 = Object();
+    BN1.element = document.getElementById("BN1");
+    var BN2 = Object();
+    BN2.element = document.getElementById("BN2");
+    
+    var BR1 = Object();
+    BR1.element = document.getElementById("BR1");
+    var BR2 = Object();
+    BR2.element = document.getElementById("BR2");
+    
+    var WK = Object();
+    WK.element = document.getElementById("WK");
+    var WQ = Object();
+    WQ.element = document.getElementById("WQ");
+    
+    var WB1 = Object();
+    WB1.element = document.getElementById("WB1");
+    var WB2 = Object();
+    WB2.element = document.getElementById("WB2");
+    
+    var WP1 = Object();
+    WP1.element = document.getElementById("WP1");
+    var WP2 = Object();
+    WP2.element = document.getElementById("WP2");
+    var WP3 = Object();
+    WP3.element = document.getElementById("WP3");
+    var WP4 = Object();
+    WP4.element = document.getElementById("WP4");
+    var WP5 = Object();
+    WP5.element = document.getElementById("WP5");
+    var WP6 = Object();
+    WP6.element = document.getElementById("WP6");
+    var WP7 = Object();
+    WP7.element = document.getElementById("WP7");
+    var WP8 = Object();
+    WP8.element = document.getElementById("WP8");
+    
+    var WN1 = Object();
+    WN1.element = document.getElementById("WN1");
+    var WN2 = Object();
+    WN2.element = document.getElementById("WN2");
+    
+    var WR1 = Object();
+    WR1.element = document.getElementById("WR1");
+    var WR2 = Object();
+    WR2.element = document.getElementById("WR2");
+    
+    blacks = [BR1, BN1, BB1, BQ, BK, BB2, BN2, BR2, BP1, BP2, BP3, BP4, BP5, BP6, BP7, BP8];
+    whites = [WR1, WN1, WB1, WQ, WK, WB2, WN2, WR2, WP1, WP2, WP3, WP4, WP5, WP6, WP7, WP8];
+    squareFun = function(row, col){ //bottom left is 1,1. 0,0 is off the board.
+        if(row === 0 || col === 0){
+            this.element.style.display = 'none';
+        }else{
+            this.element.style.top = (15+(8-row)*61.5)+'px';
+            this.element.style.left = (15+(col-1)*61.5)+'px';
+        }
+    };
+    
+    for(var i=0; i<2; i++){
+        for(var j=0; j<8; j++){
+            whites[i*8+j].setSquare = squareFun;
+            whites[i*8+j].setSquare(i+1, j+1);
+            whites[i*8+j].pos = [[i+1, j+1]];
+        }
     }
-  }
-  for(var i=0; i<2; i++){
-    for(var j=0; j<8; j++){
-        blacks[i*8+j].setSquare = squareFun;
-        blacks[i*8+j].setSquare(8-i, j+1);
-        blacks[i*8+j].pos = [[8-i, j+1]];
+    for(var i=0; i<2; i++){
+        for(var j=0; j<8; j++){
+            blacks[i*8+j].setSquare = squareFun;
+            blacks[i*8+j].setSquare(8-i, j+1);
+            blacks[i*8+j].pos = [[8-i, j+1]];
+        }
     }
-  }
 }
 
 function reset(){
@@ -137,14 +137,14 @@ function reset(){
     document.getElementById("status").innerHTML = "";
     document.getElementById("debug").innerHTML = "";
     occupied = [[1,1,1,1,1,1,1,1],
-                           [1,1,1,1,1,1,1,1],
-                           [0,0,0,0,0,0,0,0],
-                           [0,0,0,0,0,0,0,0],
-                           [0,0,0,0,0,0,0,0],
-                           [0,0,0,0,0,0,0,0],
-                           [2,2,2,2,2,2,2,2],
-                           [2,2,2,2,2,2,2,2]
-                           ];
+                [1,1,1,1,1,1,1,1],
+                [0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0],
+                [2,2,2,2,2,2,2,2],
+                [2,2,2,2,2,2,2,2]
+                ];
 }
 function example(){
     document.getElementById("pgn").value = "1.e4 e5 2.Nf3 Nc6 3.Bb5 Nd4 4.Nxd4 exd4 5.c3 Qg5 6.O-O Qxb5 7.cxd4 Nf6 8.Nc3 Qc4 9.e5 Nd5 10.Nxd5 Qxd5 11.d3 d6 12.exd6 Bxd6 13.Re1+ Be6 14.Qa4+ c6 15.Be3 O-O 16.Qd1 h6 17.Qc1 Bb4 18.Re2 Rfe8 19.Bxh6 gxh6 20.Qxh6 Bf8 21.Qf6 Qf5 22.Qh4 Bd5 23.Qg3+ Qg6 24.Qxg6+ fxg6 25.Re5 Rxe5 26.dxe5 Bg7 27.d4 b6 28.f4 c5 29.dxc5 bxc5 30.b3 c4 31.Rd1 c3 32.Rc1 Rc8 33.g3 c2 34.Kf2 Bf8 35.Ke2 Ba3 36.Rxc2 Rxc2+ 37.Kd3 Rxh2 ";
@@ -166,43 +166,43 @@ function resetBoard(){
             blacks[i*8+j].setSquare(8-i, j+1);
             blacks[i*8+j].pos = [[8-i, j+1]];
             blacks[i*8+j].element.style.display = "inline";
-
+            
         }
     }
-
+    
 }
 
 function pgnSubmit(){
     
     resetBoard();
-	var pgn = document.getElementById("pgn").value;
-	var arr = pgn.split("]");
+    var pgn = document.getElementById("pgn").value;
+    var arr = pgn.split("]");
     if(arr.length<1){
         document.getElementById("status").innerHTML = "Invalid PGN!";
         hideButtons();
         return;
     }
-	var moves = arr[arr.length-1];
+    var moves = arr[arr.length-1];
     if(!isValid(moves)){
         document.getElementById("status").innerHTML = "Invalid PGN!";
         hideButtons();
         return;
     }
-	moves = moves.split(".");
-	//I'm new to this, so maybe a lame way to do it
-	moves = moves.slice(1, moves.length); //remove first number
+    moves = moves.split(".");
+    //I'm new to this, so maybe a lame way to do it
+    moves = moves.slice(1, moves.length); //remove first number
     
-	var move;
-	for(var i=0; i<moves.length; i++){
-		move = moves[i];
-		moves[i] =
+    var move;
+    for(var i=0; i<moves.length; i++){
+        move = moves[i];
+        moves[i] =
         move.slice(0,move.length-1).trim().split(" ");
-	}
+    }
     
     
-	if(parsePGN(moves)){
+    if(parsePGN(moves)){
         document.getElementById("status").innerHTML = "Parse Successful!";
-		showButtons();
+        showButtons();
     }else{
         document.getElementById("status").innerHTML = "Invalid PGN!";
         hideButtons();
@@ -269,16 +269,14 @@ function reverse(){
 
 function parsePGN(moves){
     //each element of moves is an array of 2 strings, each of which is a move.
-    var tableHTML = document.getElementById("table");
     last_move = moves.length*2; //don't worry about this until later.
     var move;
     updatePotentialMoves();
     for(var i = 0; i< moves.length-1; i++){
         //the update functions trust the move_IDX to signify current positions
         move = moves[i];
-        tableHTML.innerHTML += "<tr><td>" + ((move_IDX/2)+1) + ". "
-                            + move[0] + "</td><td>" + move[1]
-                            + "</td></tr>";
+        
+        tableAdd(move[0], move[1], move_IDX);
         
         if(!makeMove( move[0].replace(/\+|#/,""), 1)){
             return false;
@@ -292,7 +290,7 @@ function parsePGN(moves){
         updatePotentialMoves();
     }
     move = moves[moves.length-1];
- 
+    
     if(!makeMove( move[0].replace(/\+|#/,""), 1)){
         return false;
     }
@@ -301,38 +299,33 @@ function parsePGN(moves){
     
     if(move.length>1){
         var firstchar = move[1].charCodeAt(0);
-        report(firstchar);
-        if( !isCastle( move[1].replace(/\+|#/,"") ) && (firstchar<65||firstchar>122) ){
-//            //its just the score
+       if( !isCastle( move[1].replace(/\+|#/,"") ) && (firstchar<65||firstchar>122) ){
+            //            //its just the score
             report("Score detected");
             last_move--;
-            tableHTML.innerHTML += "<tr><td>" + (move_IDX+1) + ".   "
-            + move[0] + "</td></tr>";
+            tableAdd(move[0], " ", move_IDX-1);
+            
         }else{
             updatePotentialMoves();
             if(!makeMove( move[1].replace(/\+|#/,""), 2)){
                 return false;
             }
-            tableHTML.innerHTML += "<tr><td>" + ((move_IDX+1)/2) + ".   "
-                + move[0] + "</td><td>" + move[1]
-                + "</td></tr>";
+            tableAdd(move[0], move[1], move_IDX-1)
         }
-    }else{
+    }else{//actually, this might never happen if score is always included. Whatever.
         last_move--;
-        tableHTML.innerHTML += "<tr><td>" + (move_IDX+1) + ".   "
-        + move[0] + "</td></tr>";
-
+        tableAdd(move[0], " ", move_IDX-1);
+        
     }
     //document.getElementById("debug").innerHTML = debugInfo();
     move_IDX = 0;
     return true;
-
+    
 }
 function makeMove(move, color){ //needs to return false for impossible move
     //assumption: move_IDX refers to locations of pieces
     //before move. Potentials are set up. New positions to be added
     //at index move_IDX + 1
-    report(move);
     for(var i=0; i<16; i++){
         whites[i].pos[move_IDX+1] = whites[i].pos[move_IDX];
         blacks[i].pos[move_IDX+1] = blacks[i].pos[move_IDX];
@@ -372,7 +365,7 @@ function makeMove(move, color){ //needs to return false for impossible move
                 moving_piece = pieceOnSquare([sourceRow,sourceCol], blacks);
             }
             remove = pieceOnSquare(takenPawnPos, taken_color);
-          
+            
         }
         remove.pos[move_IDX+1] = [0,0]; //
     }
@@ -382,7 +375,7 @@ function makeMove(move, color){ //needs to return false for impossible move
     var oldPos = moving_piece.pos[move_IDX]; //GOOD
     moving_piece.pos[move_IDX+1] = newPos;
     occupied[newPos[0]-1][newPos[1]-1] = color;
-  
+    
     occupied[oldPos[0]-1][oldPos[1]-1] = 0;
     return true;
     
@@ -390,8 +383,8 @@ function makeMove(move, color){ //needs to return false for impossible move
 function whichPiece(move, newPos, color){
     //based on first letter and location of move
     var code = move.charCodeAt(0);
- 
-    var movers;
+    
+    var movers=[0];
     if(code>96){//aka lower case, aka pawn
         movers = whichOfThem(newPos, color, [8,9,10,11,12,13,14,15]);
     }else{
@@ -412,12 +405,12 @@ function whichPiece(move, newPos, color){
                 movers = whichOfThem(newPos, color, [0, 7]);
                 break;
             default:
-                alert("ERROR Which piece?");
+                report("WHICH PIECE?");
                 break;
         }
     }
     if(movers.length>1){
-       
+        
         return settleAmbiguity(move, newPos, color, movers);
     }
     return movers[0];
@@ -435,11 +428,11 @@ function whichOfThem(pos, color, nums){
     
     for(var j=0; j<nums.length; j++){
         num = nums[j];
-      
+        
         piece = pieces[num];
         for(var i = 0; i < piece.potential.length; i++){
             potpos = piece.potential[i];
-        
+            
             if(potpos[0]==pos[0] && potpos[1]==pos[1]){
                 correctPieces.push(piece);
             }
@@ -468,7 +461,7 @@ function isCastle(move){
     return move=="O-O"||move=="O-O-O";
 }
 function processCastle(move, color){
-
+    
     if(move=="O-O"){
         if(color==1){
             whites[4].pos[move_IDX+1] = [1,7];
@@ -484,7 +477,7 @@ function processCastle(move, color){
             occupied[7][5] = 2;
             occupied[7][6] = 2;
             occupied[7][7] = 0;
-
+            
         }
     }else{
         if(color==1){
@@ -495,7 +488,7 @@ function processCastle(move, color){
             occupied[0][2] = 1;
             occupied[0][3] = 1;
             occupied[0][4] = 0;
-
+            
         }else{
             blacks[4].pos[move_IDX+1] = [8,3];
             blacks[0].pos[move_IDX+1] = [8,2];
@@ -549,7 +542,7 @@ function settleAmbiguity( move, newPos, color, movers){
                     return movers[i];
                 }
             }
-
+            
         }
         
     }
@@ -567,39 +560,57 @@ function settleAmbiguity( move, newPos, color, movers){
 // DISPLAYING MOVES
 // TODO: NOTHING
 function leftClick(){
-    
-	if(move_IDX > 0){
-		move_IDX--;
-		updatePieces();
-	}
+    if(move_IDX > 0){
+        move_IDX--;
+        updatePieces();
+        
+        if(move_IDX>0){
+            document.getElementById(""+(move_IDX-1)).style.background = "#00FFFF";
+        }
+        document.getElementById(""+(move_IDX)).style.background = "white";
+        
+        
+        
+    }
 }
 function rightClick(){
-	if(move_IDX < last_move){
-		move_IDX++;
-		updatePieces();
-	}
+    if(move_IDX < last_move){
+        move_IDX++;
+        updatePieces();
+        
+        if(move_IDX>1){
+            document.getElementById(""+(move_IDX-2)).style.background = "white";
+        }
+        document.getElementById(""+(move_IDX-1)).style.background = "#00FFFF";
+        
+        
+        
+        
+    }
 }
+
+
 function updatePieces(){
-	var pos, piece;
-	for(var i=0; i<16;i++){
-       
-		piece = whites[i];
-		if(move_IDX==piece.pos.length){ //TODO THIS MAY BE OBSOLETE METHOD
-			piece.setSquare(0,0);
-		}else if(move_IDX<piece.pos.length){
-			piece.element.style.display = "inline";
+    var pos, piece;
+    for(var i=0; i<16;i++){
+        
+        piece = whites[i];
+        if(move_IDX==piece.pos.length){ //TODO THIS MAY BE OBSOLETE METHOD
+            piece.setSquare(0,0);
+        }else if(move_IDX<piece.pos.length){
+            piece.element.style.display = "inline";
             pos = piece.pos[move_IDX];
-			piece.setSquare(pos[0], pos[1]);
-		}
-		piece = blacks[i];
-		if(move_IDX==piece.pos.length){
-			piece.setSquare(0,0);
-		}else if(move_IDX<piece.pos.length){
-			piece.element.style.display = "inline";
-			pos = piece.pos[move_IDX];
-			piece.setSquare(pos[0], pos[1]);
-		}
-	}
+            piece.setSquare(pos[0], pos[1]);
+        }
+        piece = blacks[i];
+        if(move_IDX==piece.pos.length){
+            piece.setSquare(0,0);
+        }else if(move_IDX<piece.pos.length){
+            piece.element.style.display = "inline";
+            pos = piece.pos[move_IDX];
+            piece.setSquare(pos[0], pos[1]);
+        }
+    }
 }
 
 // UPDATING POTENTIALS
@@ -611,7 +622,7 @@ function updatePotentialMoves(){
     updateQueensPotentialMoves();
     updateKingsPotentialMoves();
     updatePawnsPotentialMoves();
-
+    
 }
 function updateRooksPotentialMoves(){
     updateRookPotentialMoves(whites[0], 1);
@@ -850,8 +861,13 @@ function updatePawnPotentialMoves(pawn, color){
 }
 
 
+function tableAdd(move1, move2, index){
+    document.getElementById("table").innerHTML +=
+    "<tr><td id=\""+index+"\" >" + ((index/2)+1) + ". "+ move1 + "</td><td id=\""+(index+1)+"\" >" + move2 + "</td></tr>";
+}
+
 function report(statement){
-	//for debugging - disabled on my website!
+    //for debugging - disabled on my website!
     
     //document.getElementById("debug").innerHTML +=(statement+"<br />");
     
@@ -863,18 +879,18 @@ function report(statement){
  checking
  
  Anything involving promotion. This should require a big update.
-
-*/
+ 
+ */
 
 /*checking:
- potential moves need to be known to identify which piece of a certain type 
+ potential moves need to be known to identify which piece of a certain type
  was moved to a certain square. If there is an ambiguity, THEN we can check whether one
  of those moves would put the King in check. */
 /*Thoughts on potential moves: This can give rise to a graphic that allows
  the user to constantly know the reach of a certain piece as the game goes on.
  Applications to a potential expansion of this project into a chess engine are
  obvious*/
- 
+
 
 
 
